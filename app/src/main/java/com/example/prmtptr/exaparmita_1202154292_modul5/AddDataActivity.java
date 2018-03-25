6dae807935ceba84da9dd42570b8482c0686738d
prmtptr <prmtptr@gmail.com>
2018-03-25 21:24:59 +0700

    bismilah modul 5

---
+++ app/src/main/java/com/example/prmtptr/exaparmita_1202154292_modul5/AddDataActivity.java
@@ -0,0 +1,43 @@
+package com.example.prmtptr.exaparmita_1202154292_modul5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDataActivity {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddDataActivity(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
+
+
